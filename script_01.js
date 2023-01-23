f3e52bbb5707e3aee515e43535d52a7e0353c455
Lawrence <lawrence.langen@gmail.com>
2023-01-23 09:45:38 +0100

    Nested_Arrays

--- script_01.js
+++ script_01.js
@@ -1,11 +1,32 @@
 
 /***** Erinnerung: Arrays *******/
 
-// let arr;
-// arr = ["Ich","bin","Max"];
-// output(arr);
-// output(arr[0]);
-// output("------------");
+let arr;
+arr = ["Ich","bin","Max"];
+output(arr);
+output(arr[0]);
+output("------------");
+
+// nested arrays - Prinzip: [[[]]]
+// nested arrays - hier: [[],[]]
+arr =   [
+                 ["Ich","bin","Max"],
+                 ["Ich","bin","Moritz"]
+         ];
+
+
+output(arr);
+output(arr[0]);
+output(arr[0][0] + " " + arr[0][1] + " " + arr[0][2]);
+output(arr[1][2]);
+
+//nested loop
+// for (let i = 0; i < arr.length; i++) {
+//         const element = arr[i];
+        
+// }
+        
+
 
 
 /***** Objekte 1 Daten/Funktionen *******/
